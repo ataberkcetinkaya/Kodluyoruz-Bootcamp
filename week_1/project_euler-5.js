f0df022ae1929dbb763fa40febc257994dc6620c
ataberkcetinkaya <chpataberk@outlook.com.tr>
2022-06-22 12:20:47 +0300

    English Explanation & Variables

--- week_1/project_euler-5.js
+++ week_1/project_euler-5.js
@@ -1,15 +1,15 @@
-function euler5(sayi) {
-    const carpanlar = []; //bos bir array tanimlariz
+function euler5(num) {
+    const mults = []; //create an empty array name called mults.
     
-    for(let i = 1; i <= sayi; i++) { //1'den hedefledigimiz sayiya kadar dongu olustururuz
-        let dgrCarpanlar = i; //dgrCarpanlar degiskenine dolastigimiz sayiyi atiyoruz
-        carpanlar.forEach(function(crpnlr) { //forEach dongusu ile carpanlar arrayini gezecek
-            if(dgrCarpanlar % crpnlr == 0) { //carpanlar arrayindeki sayi ile dgrCarpanlar'daki sayi bolunebilir mi diye bakacak
-                dgrCarpanlar /= crpnlr; //oyle ise dgrCarpanlar'daki sayi carpanlar arrayindeki sayiyi bolecek
+    for(let i = 1; i <= num; i++) { //loop through the numbers starting from 1
+        let newMult = i; //discard the number in which we circulate the variable newMult
+        mults.forEach(function(mult) { //will traverse the array of mults with the forEach return
+            if(newMult % mult == 0) { //check if the number is divisible by the number in the array
+                newMult /= mult; //if it is, divide the number by the number in the array
             }
         })
-        carpanlar.push(dgrCarpanlar); //carpanlar arrayine push ederiz
+        mults.push(newMult); //push the number in the array
     }
-    return carpanlar.reduce((nmbr = 1, crpnlr) => nmbr *= crpnlr); //arrayi tek sayiya dusururuz ve return ederiz.
+    return mults.reduce((nmbr = 1, mult) => nmbr *= mult); //reduce the array to the nmbr of all the numbers
 }
 console.log(euler5(20));
