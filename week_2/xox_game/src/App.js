418e2c215eddc923d77d0a9dc8942796f35ff594
ataberkctnky <ataberkcetinkaya@outlook.com>
2022-07-03 09:21:40 -0300

    the XoX Game - v2

--- week_2/xox_game/src/App.js
+++ week_2/xox_game/src/App.js
@@ -1,93 +1,93 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import Box from './components/Box';
-
-const board = [[], [], []];
+import './new.css';
+import { useState } from 'react';
 
 function App() {
 
-   const [turn, setTurn] = useState('X');
    const [winner, setWinner] = useState('');
+   const [cells, setCells] = useState(Array(9).fill(''));
+   const [theCell, setTheCell] = useState(Array(9).fill('normal'));
 
-   function changeTurn(row, col) {
-     setTurn(turn === 'X' ? 'O' : 'X');
+   let signs = {
+      horizontal: [[0, 1, 2], [3, 4, 5], [6, 7, 8]],
+      vertical: [[0, 3, 6], [1, 4, 7], [2, 5, 8]],
+      diagonal: [[0, 4, 8], [2, 4, 6]]
+   };
 
-     board[row][col] = turn;
-     let w = checkForWinner();
-
-     if(!w) {
-
-     } else {
-      setWinner(w + ' WON!');
-     }
-    }
-
-    function checkForWinner() {
-      //check winner f rows
-      for(let i = 0; i < board.length; i++) {
-        const row = board[i];
-
-        if(row[0] === row[1] && row[1] === row[2] && row[0]) {
-          return row[0];
-        }
-    }
-
-      //check winner f columns
-      for(let i = 0; i < board.length; i++) {
-        const cl0 = board[0][i];
-        const cl1 = board[1][i];
-        const cl2 = board[2][i];
-      
-        if(cl0 === cl1 && cl1 === cl2 && cl0) {
-          return cl0;
-        }
-      }
-
-      //check winner f diagonals
-      const r0 = board[0][0];
-      const r1 = board[1][1];
-      const r2 = board[2][2];
-
-      if(r0 === r1 && r1 === r2 && r0) {
-        return r0;
-      }
-
-      const l0 = board[0][2];
-      const l1 = board[1][1];
-      const l2 = board[2][0];
-
-      if(l0 === l1 && l1 === l2 && l0) {
-        return l0;
-      }
-
-      return false;
-
-    }
-
+   const checkForWinner = (squares) => {
+		for (let sign in signs) {
+			signs[sign].forEach((item) => {
+				if (
+					squares[item[0]] === '' || squares[item[1]] === '' || squares[item[2]] === ''
+				) {
+				} else if (
+					squares[item[0]] === squares[item[1]] && squares[item[1]] === squares[item[2]]
+				) {
+					setWinner(squares[item[0]]);
+				}
+			});
+		}
+	};
+	let sqrs = [...theCell];
+	const handleClick = (num) => {
+		if (winner === ''){
+			let squares = [...cells];
+			if (cells[num] === '') {
+				squares[num] = 'x';
+				sqrs[num] = 'normal' + ' xli';
+			} else if (cells[num] === 'x') {
+				squares[num] = 'o';
+				sqrs[num] = 'normal' + ' olu';
+			} else {
+				squares[num] = '';
+				sqrs[num] = 'normal';
+			}
+			for (let sign in signs) {
+				signs[sign].forEach((item) => {
+					if (
+						sqrs[item[0]] === 'normal' || sqrs[item[1]] === 'normal' || sqrs[item[2]] === 'normal'
+					) {
+					} else if (
+						sqrs[item[0]] === sqrs[item[1]] && sqrs[item[1]] === sqrs[item[2]] && squares[item[0]] === 'x'
+					) {
+						sqrs[item[0]] = 'normal' + ' xli' + ' winnerx';
+						sqrs[item[1]] = 'normal' + ' xli' + ' winnerx';
+						sqrs[item[2]] = 'normal' + ' xli' + ' winnerx';
+					} else if (
+						sqrs[item[0]] === sqrs[item[1]] && 
+						sqrs[item[1]] === sqrs[item[2]] &&
+						squares[item[0]] === 'o'
+					) {
+						sqrs[item[0]] = 'normal' + ' olu' + ' winnero';
+						sqrs[item[1]] = 'normal' + ' olu' + ' winnero';
+						sqrs[item[2]] = 'normal' + ' olu' + ' winnero';
+					}
+				});
+			}
+			checkForWinner(squares);
+			setCells(squares);	
+			setTheCell(sqrs);	
+		} else {
+				setWinner('');
+				setCells(Array(9).fill(''));
+				setTheCell(Array(9).fill('normal'));
+		}
+	};
+  
   return (
     <div class="container">
-    <h3 class="gameText">
-      
-      GAME TIME
-     
-      
-      <div id="wnnr">{winner}</div>
-
-      <div id="change" class="bla noSelect">
-         <Box row={0} col={0} currentState={turn} changeTurn={changeTurn} />
-         <Box row={0} col={1} currentState={turn} changeTurn={changeTurn} />
-         <Box row={0} col={2} currentState={turn} changeTurn={changeTurn} />
-         <Box row={1} col={0} currentState={turn} changeTurn={changeTurn} />
-         <Box row={1} col={1} currentState={turn} changeTurn={changeTurn} />
-         <Box row={1} col={2} currentState={turn} changeTurn={changeTurn} />
-         <Box row={2} col={0} currentState={turn} changeTurn={changeTurn} />
-         <Box row={2} col={1} currentState={turn} changeTurn={changeTurn} />
-         <Box row={2} col={2} currentState={turn} changeTurn={changeTurn} />
-         
-          
-      </div> 
-      </h3>
-      </div>
+        <p class="gametxt">GAME TIME</p>
+        <div class="boxes noSelect">
+          <div className={theCell[0]} onClick={() => handleClick(0)}>{cells[0]}</div>
+          <div className={theCell[1]} onClick={() => handleClick(1)}>{cells[1]}</div>
+          <div className={theCell[2]} onClick={() => handleClick(2)}>{cells[2]}</div>
+          <div className={theCell[3]} onClick={() => handleClick(3)}>{cells[3]}</div>
+          <div className={theCell[4]} onClick={() => handleClick(4)}>{cells[4]}</div>
+          <div className={theCell[5]} onClick={() => handleClick(5)}>{cells[5]}</div>
+          <div className={theCell[6]} onClick={() => handleClick(6)}>{cells[6]}</div>
+          <div className={theCell[7]} onClick={() => handleClick(7)}>{cells[7]}</div>
+          <div className={theCell[8]} onClick={() => handleClick(8)}>{cells[8]}</div>
+        </div>		
+    </div>
   );
 }
 
